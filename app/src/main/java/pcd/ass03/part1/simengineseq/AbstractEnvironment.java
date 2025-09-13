5f52b04860497680c7ef17dd14abec3b3ba68418
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-09-13 12:17:27 +0200

    fig bug

--- app/src/main/java/pcd/ass03/part1/simengineseq/AbstractEnvironment.java
+++ app/src/main/java/pcd/ass03/part1/simengineseq/AbstractEnvironment.java
@@ -52,7 +52,7 @@ public abstract class AbstractEnvironment extends Thread {
 	public abstract void doAction(String agentId, Action act);
 
 	public void setnSteps(int numSteps) {
-		this.nSteps = nSteps;
+		this.nSteps = numSteps;
 	}
 
 	public int getnSteps() {
