bb3716c69523e172315256d7dbd54129c7a804c3
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-09-13 11:04:47 +0200

    update messages

--- app/src/main/java/pcd/ass03/part1/simtrafficbase/messages/TrafficLightReady.java
+++ app/src/main/java/pcd/ass03/part1/simtrafficbase/messages/TrafficLightReady.java
@@ -2,4 +2,4 @@ package pcd.ass03.part1.simtrafficbase.messages;
 
 import akka.actor.typed.ActorRef;
 
-public record TrafficLightReady(TrafficLightReady state, ActorRef<Message> sender) implements Message{}
+public record TrafficLightReady(ActorRef<Message> sender) implements Message{}
