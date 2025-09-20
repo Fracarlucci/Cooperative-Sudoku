69bb82ad7d87904cba9959a89717473c106b9d58
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-09-20 12:10:17 +0200

    dekete useless part

--- app/src/main/java/pcd/ass03/part2A/view/SudokuGUI.java
+++ app/src/main/java/pcd/ass03/part2A/view/SudokuGUI.java
@@ -330,15 +330,4 @@ public class SudokuGUI extends JFrame {
         }
     }
     
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            
-            new SudokuGUI().setVisible(true);
-        });
-    }
 }
