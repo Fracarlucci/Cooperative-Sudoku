b3028a43a4814408c65e1ac1b857d79ebcfebfe8
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-09-26 15:30:19 +0200

    create channelsEnum

---
+++ app/src/main/java/pcd/ass03/part2A/model/ChannelsEnum.java
@@ -0,0 +1,24 @@
+package pcd.ass03.part2A.model;
+
+public enum ChannelsEnum {
+  CHANNEL_CREATE_SUDOKU("channel_create_sudoku"),
+  CHANNEL_UPDATE_SUDOKU("channel_update_sudoku"),
+  CHANNEL_SELECT_CELL("channel_select_cell"),
+  CHANNEL_UNSELECT_CELL("channel_unselect_cell"),
+  CHANNEL_SET_VALUE("channel_set_value");
+
+   private final String name;
+
+    ChannelsEnum(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
