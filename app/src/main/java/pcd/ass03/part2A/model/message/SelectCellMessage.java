849b4e6778792bbbdc14672e218e64bfc33ca60f
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-09-26 16:32:18 +0200

    create unselect deserializer and fix bug

--- app/src/main/java/pcd/ass03/part2A/model/message/SelectCellMessage.java
+++ app/src/main/java/pcd/ass03/part2A/model/message/SelectCellMessage.java
@@ -1,3 +1,3 @@
 package pcd.ass03.part2A.model.message;
 
-public record SelectCellMessage(int sudokuId, String playerId, int row, int col) {}
+public record SelectCellMessage(int sudokuId, String playerId, int row, int col, String color) {}
