b6aa84da8db232a00dae2cda7102e1b3a0106897
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-09-26 14:24:43 +0200

    delete useless main

--- app/src/main/java/pcd/ass03/part2A/view/SudokuGUI.java
+++ app/src/main/java/pcd/ass03/part2A/view/SudokuGUI.java
@@ -604,16 +604,4 @@ public class SudokuGUI extends JFrame {
             this.playerCount = playerCount;
         }
     }
-    
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            
-            new SudokuGUI().setVisible(true);
-        });
-    }
 }
