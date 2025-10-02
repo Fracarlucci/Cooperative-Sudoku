553cddc4f49c07e8ae0f33312b06406ecafdeaa1
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-10-02 13:04:20 +0200

    update controller interface

--- app/src/main/java/pcd/ass03/part2A/controller/SudokuController.java
+++ app/src/main/java/pcd/ass03/part2A/controller/SudokuController.java
@@ -3,11 +3,11 @@ package pcd.ass03.part2A.controller;
 import pcd.ass03.part2A.model.Player;
 
 public interface SudokuController {
-  public boolean selectCell(Player player, int row, int col);
-  public boolean setCellValue(Player player, int value);
-  public void clearCellValue(Player player);
-  public void joinGame(Player player);
-  public void leaveGame(Player player);
+  public boolean selectCell(int row, int col);
+  public boolean setCellValue(int value);
+  public void clearCellValue();
+  public void joinGame(String sudokuId);
+  public void leaveGame();
   public void newGame();
   public void updateView();
 }
