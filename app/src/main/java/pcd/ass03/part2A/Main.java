23bf60f263a1d8ad58b4418e19fbe205274df6e9
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-10-09 15:25:13 +0200

    use gui in main

--- app/src/main/java/pcd/ass03/part2A/Main.java
+++ app/src/main/java/pcd/ass03/part2A/Main.java
@@ -3,6 +3,7 @@ package pcd.ass03.part2A;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
+import pcd.ass03.part2A.controller.SudokuControllerImpl;
 import pcd.ass03.part2A.model.Player;
 import pcd.ass03.part2A.model.SudokuFactory;
 import pcd.ass03.part2A.model.SudokuGrid;
@@ -16,24 +17,22 @@ public class Main {
         SudokuGrid sudoku2 = factory.generate(15);
         
         Player p1 = new Player("p1", "player-1");
-        Player p2 = new Player("p2", "player-2");
         
         Thread.sleep(200);
         
-        p1.createSudoku(sudoku1);
-        p2.createSudoku(sudoku2);
+        // p1.createSudoku(sudoku1);
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            SudokuGUI gui = new SudokuGUI(new SudokuControllerImpl(p1));
+            gui.setVisible(true);
+        });
         
         Thread.sleep(2000);
-        
-        System.out.println("Sudoku 1 ID: " + p1.getSudokusId());
-        System.out.println("Sudoku 2 ID: " + p2.getSudokusId());
-
-        p1.joinGrid(sudoku1.getId());
-        p1.selectCell(1, 1);
-        p2.joinGrid(sudoku2.getId());
-        p2.selectCell(4, 7);
-        p1.setValue(5);
-        p1.selectCell(3, 3);
         
     } catch (Exception e) {
         e.printStackTrace();
