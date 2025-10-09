adc603212be17a5fd83717d43c8df44e3d324b36
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-10-09 17:17:51 +0200

    update main

--- app/src/main/java/pcd/ass03/part2A/Main.java
+++ app/src/main/java/pcd/ass03/part2A/Main.java
@@ -3,6 +3,7 @@ package pcd.ass03.part2A;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
+import pcd.ass03.part2A.controller.SudokuController;
 import pcd.ass03.part2A.controller.SudokuControllerImpl;
 import pcd.ass03.part2A.model.Player;
 import pcd.ass03.part2A.model.SudokuFactory;
@@ -14,26 +15,49 @@ public class Main {
     try {
         SudokuFactory factory = new SudokuFactory();
         SudokuGrid sudoku1 = factory.generate(10);
-        SudokuGrid sudoku2 = factory.generate(15);
         
         Player p1 = new Player("p1", "player-1");
+        Player p2 = new Player("p2", "player-2");
         
         Thread.sleep(200);
-        
-        // p1.createSudoku(sudoku1);
 
-        SwingUtilities.invokeLater(() -> {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            SudokuGUI gui = new SudokuGUI(new SudokuControllerImpl(p1));
-            gui.setVisible(true);
-        });
+        SudokuController controller1 = new SudokuControllerImpl(p1);
+        SudokuController controller2 = new SudokuControllerImpl(p2);
+        p1.setController(controller1);
+        p2.setController(controller2);
+
+        SudokuGUI gui = new SudokuGUI(controller1);
+        SudokuGUI gui2 = new SudokuGUI(controller2);
+        ((SudokuControllerImpl)controller1).setView(gui);
+        ((SudokuControllerImpl)controller2).setView(gui2);
+        gui.setVisible(true);
+        gui2.setVisible(true);
         
-        Thread.sleep(2000);
-        
+        // ((SudokuControllerImpl)controller1).setView(gui);
+        // ((SudokuControllerImpl)controller2).setView(new SudokuGUI(controller2));
+
+        // SwingUtilities.invokeLater(() -> {
+        //     try {
+        //         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        //     } catch (Exception e) {
+        //         e.printStackTrace();
+        //     }
+        //     SudokuGUI gui = new SudokuGUI();
+        //     gui.setVisible(true);
+        // });
+        // SwingUtilities.invokeLater(() -> {
+        //     try {
+        //         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        //     } catch (Exception e) {
+        //         e.printStackTrace();
+        //     }
+        //     SudokuGUI gui = new SudokuGUI(new SudokuControllerImpl(p2));
+        //     gui.setVisible(true);
+        // });
+
+        // SudokuGUI gui2 = new SudokuGUI(new SudokuControllerImpl(p2));
+        // gui2.setVisible(true);
+                
     } catch (Exception e) {
         e.printStackTrace();
     }
