76b12662c792e39a261570573e3ff20332a2d5b5
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-10-14 14:50:28 +0200

    add method in user callback interface

--- app/src/main/java/pcd/ass03/part2B/model/UserCallbackInterface.java
+++ app/src/main/java/pcd/ass03/part2B/model/UserCallbackInterface.java
@@ -4,5 +4,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface UserCallbackInterface extends Remote {
+    String getPlayerId() throws RemoteException;
     public void notifyUser(SudokuGrid sudoku) throws RemoteException; 
 }
