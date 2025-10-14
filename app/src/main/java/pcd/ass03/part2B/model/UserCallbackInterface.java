6a8c7e24e51a0e56b7fcb1da580082f67d81755a
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-10-14 10:49:32 +0200

    create user callback

---
+++ app/src/main/java/pcd/ass03/part2B/model/UserCallbackInterface.java
@@ -0,0 +1,8 @@
+package pcd.ass03.part2B.model;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface UserCallbackInterface extends Remote {
+    public void notifyUser(SudokuGrid sudoku) throws RemoteException; 
+}
