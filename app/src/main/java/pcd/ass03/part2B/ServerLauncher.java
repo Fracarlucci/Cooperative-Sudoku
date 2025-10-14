42af1e18fc7f684b35b31e3077cc1eebf1c5741a
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-10-14 11:42:36 +0200

    change launchers name and fix it for testing

---
+++ app/src/main/java/pcd/ass03/part2B/ServerLauncher.java
@@ -0,0 +1,25 @@
+package pcd.ass03.part2B;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import pcd.ass03.part2B.controller.SudokuController;
+import pcd.ass03.part2B.model.Player;
+import pcd.ass03.part2B.model.Server;
+import pcd.ass03.part2B.model.ServerImpl;
+
+public class ServerLauncher {
+    public static void main(String[] args) {
+         try {
+            Server server = new ServerImpl();
+            Server serverStub = (Server) UnicastRemoteObject.exportObject(server, 0);
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.rebind("SudokuServer", serverStub);
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
