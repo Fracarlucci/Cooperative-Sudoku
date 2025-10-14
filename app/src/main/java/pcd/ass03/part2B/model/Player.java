92e842d2860c5fbbfa1562174cb90931ae888f6d
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-10-14 10:49:01 +0200

    adapt player to JavaRMI architecture

--- app/src/main/java/pcd/ass03/part2B/model/Player.java
+++ app/src/main/java/pcd/ass03/part2B/model/Player.java
@@ -2,6 +2,10 @@ package pcd.ass03.part2B.model;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
@@ -11,20 +15,19 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
+import pcd.ass03.part2A.utils.MessageUtils;
 import pcd.ass03.part2B.controller.SudokuController;
 import pcd.ass03.part2B.model.message.SelectCellMessage;
 import pcd.ass03.part2B.model.message.SetValueMessage;
 import pcd.ass03.part2B.model.message.UnselectCellMessage;
-import pcd.ass03.part2B.utils.MessageUtils;
 
-public class Player {
+public class Player extends UnicastRemoteObject implements UserCallbackInterface {
     private static final int GRID_SIZE = 9;
     
-    private final List<SudokuGrid> sudokus = new ArrayList<>();
-    private Channel channel;
-    private Connection connection;
+    private SudokuGrid sudoku;
     private final String playerId;
     private final String playerName;
+    private final Server server;
     private final String color = String.format("#%06x", (int)(Math.random() * 0xFFFFFF));
     private volatile String currentGridId;
     private volatile int selectedRow = -1;
@@ -32,137 +35,47 @@ public class Player {
 
     private SudokuController controller;
 
-    public Player(String playerId, String playerName) throws IOException, TimeoutException, InterruptedException {
+    public Player(String playerId, String playerName) throws RemoteException {
         this.playerId = playerId;
         this.playerName = playerName;
-        
-        this.setupConnection();
-        this.setupExchangesAndConsumers();
+        this.sudoku = null;
+        try {
+            Registry registry = LocateRegistry.getRegistry();
+
+            this.server = (Server) registry.lookup("SudokuServer");
+            this.server.registerPlayer(this); // registra il player al server, per avere le callback
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to connect to RMI server", e);
+        }
     }
 
     public void setController(SudokuController controller) {
         this.controller = controller;
     }
 
-    // Create the necessary exchanges and consumers for RabbitMQ communication.
-    private void setupExchangesAndConsumers() throws IOException, InterruptedException {
-        channel.exchangeDeclare(ChannelsEnum.CHANNEL_CREATE_SUDOKU.getName(), "fanout");
-        channel.exchangeDeclare(ChannelsEnum.CHANNEL_SELECT_CELL.getName(), "fanout");
-        channel.exchangeDeclare(ChannelsEnum.CHANNEL_UNSELECT_CELL.getName(), "fanout");
-        channel.exchangeDeclare(ChannelsEnum.CHANNEL_SET_VALUE.getName(), "fanout");
-
-        String queueName = channel.queueDeclare().getQueue();
-
-        channel.queueBind(queueName, ChannelsEnum.CHANNEL_CREATE_SUDOKU.getName(), "");
-        channel.basicConsume(queueName, true, addSudokuCallBack(), t -> {});
-
-        String selectQueue = channel.queueDeclare().getQueue();
-        channel.queueBind(selectQueue, ChannelsEnum.CHANNEL_SELECT_CELL.getName(), "");
-        channel.basicConsume(selectQueue, true, selectCellCallBack(), t -> {});
-
-        String unselectQueue = channel.queueDeclare().getQueue();
-        channel.queueBind(unselectQueue, ChannelsEnum.CHANNEL_UNSELECT_CELL.getName(), "");
-        channel.basicConsume(unselectQueue, true, unselectCellCallBack(), t -> {});
-
-        String setValueQueue = channel.queueDeclare().getQueue();
-        channel.queueBind(setValueQueue, ChannelsEnum.CHANNEL_SET_VALUE.getName(), "");
-        channel.basicConsume(setValueQueue, true, setValueCallBack(), t -> {});
-    }
-
     // MESSAGE SENDING METHODS
 
-    public void createSudoku(SudokuGrid grid) throws IOException {
-        sudokus.add(grid);
-        String message = MessageUtils.serializeSudokuGrid(grid.getId(), grid.getCreator(), grid.getGrid());
-                
-        setupConnectionIfNeeded();
-
-        channel.basicPublish(ChannelsEnum.CHANNEL_CREATE_SUDOKU.getName(), "", null, message.getBytes(StandardCharsets.UTF_8));
+    public void createSudoku() throws RemoteException {
+        SudokuGrid sudoku = server.createSudoku(playerId);
+        this.sudoku = sudoku;
+        this.currentGridId = sudoku.getId();
+        server.registerPlayerInSudoku(playerId, currentGridId);
     }
 
-    private void sendSelectCell(int row, int col) throws IOException {
-        String message = currentGridId + " " + playerId + " " + row + " " + col + " " + color;
-        setupConnectionIfNeeded();
-        channel.basicPublish(ChannelsEnum.CHANNEL_SELECT_CELL.getName(), "", null, message.getBytes(StandardCharsets.UTF_8));
+    private void sendSelectCell(int row, int col) throws RemoteException {
+        server.selectCell(new SelectCellMessage(currentGridId, playerId, row, col, color));
     }
 
-    private void sendUnselectCell(int row, int col) throws IOException {
-        String message = currentGridId + " " + playerId + " " + row + " " + col;
-        setupConnectionIfNeeded();
-
-        channel.basicPublish(ChannelsEnum.CHANNEL_UNSELECT_CELL.getName(), "", null, message.getBytes(StandardCharsets.UTF_8));
+    private void sendUnselectCell(int row, int col) throws RemoteException {
+        server.unselectCell(new UnselectCellMessage(currentGridId, playerId, row, col));
     }
 
-    private void sendSetValue(int row, int col, Integer value) throws IOException {
-        String message = currentGridId + " " + playerId + " " + row + " " + col + " " + (value == null || value < 1 || value > 9 ? -1 : value.toString());
-        setupConnectionIfNeeded();
-
-        channel.basicPublish(ChannelsEnum.CHANNEL_SET_VALUE.getName(), "", null, message.getBytes(StandardCharsets.UTF_8));
+    private void sendSetValue(int row, int col, Integer value) throws RemoteException {
+        server.setCellValue(new SetValueMessage(currentGridId, playerId, row, col, value == null || value < 1 || value > 9 ? "-1" : value.toString()));
     }
 
-    // CALLBACKS FOR MESSAGE CONSUMERS
-
-    private DeliverCallback addSudokuCallBack() {
-        return (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            SudokuGrid receivedSudoku = MessageUtils.deserializeSudokuGrid(message);
-            
-            if (sudokus.isEmpty() || sudokus.stream().noneMatch(grid -> grid.getId().equals(receivedSudoku.getId()))) {
-                sudokus.add(receivedSudoku);
-                if (controller != null) {
-                    controller.notifySudokuCreated(receivedSudoku, receivedSudoku.getCreator());
-                }
-            }
-        };
-    }
-
-    private DeliverCallback selectCellCallBack() {
-        return (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            SelectCellMessage selectCellMessage = MessageUtils.deserializeSelectCellMessage(message);
-            
-            // Ignores his own messages
-            if (!selectCellMessage.playerId().equals(this.playerId) && controller != null) {
-                controller.notifyCellSelected(selectCellMessage);
-            }
-        };
-    }
-
-    private DeliverCallback unselectCellCallBack() {
-        return (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            UnselectCellMessage unselectCellMessage = MessageUtils.deserializeUnselectCellMessage(message);
-            
-            // Ignores his own messages
-            if (!unselectCellMessage.playerId().equals(this.playerId) && controller != null) {
-                controller.notifyCellUnselected(unselectCellMessage);
-            }
-        };
-    }
-
-    private DeliverCallback setValueCallBack() {
-        return (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            SetValueMessage setValueMessage = MessageUtils.deserializeSetValueMessage(message);
-
-            // Ignores his own messages
-            if (!setValueMessage.playerId().equals(this.playerId)) {
-                sudokus.stream()
-                    .filter(grid -> grid.getId().equals(setValueMessage.sudokuId()))
-                    .findFirst()
-                    .ifPresent(grid -> {
-                        if (setValueMessage.value().equals("-1")) {
-                            grid.cancelValue(setValueMessage.row(), setValueMessage.col());
-                        } else {
-                            grid.setValue(setValueMessage.row(), setValueMessage.col(), Integer.parseInt(setValueMessage.value()));
-                        }
-
-                        if (controller != null) {
-                            controller.notifyCellValueChanged(setValueMessage);
-                        }
-                    });
-            }
-        };
+    private void sendRegisterInSudoku(String gridId) throws RemoteException {
+        server.registerPlayerInSudoku(playerId, gridId);
     }
 
     // If it is a valid move, it sets the value both locally and sends the message to the other players
@@ -170,10 +83,7 @@ public class Player {
         if (currentGridId == null) {
             throw new IllegalStateException("Player is not in a game");
         }
-        SudokuGrid currentGrid = sudokus.stream()
-                                        .filter(grid -> grid.getId().equals(currentGridId))
-                                        .findFirst()
-                                        .orElseThrow(() -> new IllegalStateException("Current grid not found"));
+        SudokuGrid currentGrid = sudoku; // TODO da modificare
         try {
             if (row != this.selectedRow || col != this.selectedCol) {
                 throw new IllegalArgumentException("Cell (" + row + "," + col + ") is not selected");
@@ -214,25 +124,6 @@ public class Player {
         this.selectedCol = -1;
     }
 
-    private void setupConnection() throws IOException, TimeoutException {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        this.connection = factory.newConnection();
-        this.channel = connection.createChannel();
-    }
-    
-    private void setupConnectionIfNeeded() throws IOException {
-        try {
-            if (channel == null || !channel.isOpen()) {
-                setupConnection();
-                // If we recreate the connection, we also need to recreate the exchanges and consumers
-                setupExchangesAndConsumers();
-            }
-        } catch (TimeoutException | InterruptedException e) {
-            throw new IOException("Failed to setup connection", e);
-        }
-    }
-
     public String getPlayerId() {
         return playerId;
     }
@@ -263,6 +154,11 @@ public class Player {
 
     public void joinGrid(String gridId) {
         this.currentGridId = gridId;
+        try {
+            sendRegisterInSudoku(gridId);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
     
     public void leaveGrid() {
@@ -274,20 +170,13 @@ public class Player {
         return color;
     }
 
-    public List<String> getSudokusId() {
-        return sudokus.stream().map(SudokuGrid::getId).toList();
-    }
-
     public PlayerInfo getPlayerInfo() {
         return new PlayerInfo(playerId, playerName, isInGame(), 
                               currentGridId, selectedRow, selectedCol);
     }
 
     public SudokuGrid getCurrentGrid() {
-        return sudokus.stream()
-                      .filter(grid -> grid.getId().equals(currentGridId))
-                      .findFirst()
-                      .orElse(null);
+        return sudoku;
     }
 
     @Override
@@ -306,5 +195,10 @@ public class Player {
         }
         
         return sb.toString();
+    }
+
+    @Override
+    public void notifyUser(SudokuGrid sudoku) throws RemoteException {
+        this.sudoku = sudoku;
     }
 }
