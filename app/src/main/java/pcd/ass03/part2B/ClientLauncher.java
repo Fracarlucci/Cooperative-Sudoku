7dbff0df26b13e886bd2cb833df5d0e9df322cef
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2025-10-15 15:47:28 +0200

    add players to client launcher

--- app/src/main/java/pcd/ass03/part2B/ClientLauncher.java
+++ app/src/main/java/pcd/ass03/part2B/ClientLauncher.java
@@ -11,34 +11,27 @@ public class ClientLauncher {
         try {      
             Player p1 = new Player("p1", "player-1");
             Player p2 = new Player("p2", "player-2");
-
-            // SudokuGrid sudokuGrid = p1.createSudoku();
-            // p2.joinGrid(sudokuGrid.getId());
-            // p2.selectCell(1, 1);
-            // p2.tryToSetValue(1, 1, 5);
-            
-            // p1.selectCell(1, 1);
-            // p1.tryToSetValue(1, 1, -1);
+            Player p3 = new Player("p3", "player-3");
             
             SudokuController controller1 = new SudokuControllerImpl(p1);
             SudokuController controller2 = new SudokuControllerImpl(p2);
-            // SudokuController controller3 = new SudokuControllerImpl(p3);
+            SudokuController controller3 = new SudokuControllerImpl(p3);
 
             SudokuGUIimpl gui1 = new SudokuGUIimpl(controller1);
             SudokuGUIimpl gui2 = new SudokuGUIimpl(controller2);
-            // SudokuGUIimpl gui3 = new SudokuGUIimpl(controller3);
+            SudokuGUIimpl gui3 = new SudokuGUIimpl(controller3);
 
             p1.setController(controller1);
             p2.setController(controller2);
-            // p3.setController(controller3);
+            p3.setController(controller3);
 
             controller1.setView(gui1);
             controller2.setView(gui2);
-            // controller3.setView(gui3);
+            controller3.setView(gui3);
 
             gui1.setVisible(true);
             gui2.setVisible(true);
-            // gui3.setVisible(true);
+            gui3.setVisible(true);
             
         } catch (Exception e) {
             e.printStackTrace();
